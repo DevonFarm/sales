3f40c0224a0685d84c5d435ec079d1f2ecc9baa4
Clark Winters <github@clarkwinters.com>
2024-05-17 23:17:02 -0500

    WIP: start of horse page with image gallery

--- go.mod
+++ go.mod
@@ -3,21 +3,11 @@ module github.com/DevonFarm/sales
 go 1.22.0
 
 require (
-	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.4 // indirect
-	github.com/gofiber/template v1.8.3 // indirect
-	github.com/gofiber/template/html/v2 v2.1.1 // indirect
-	github.com/gofiber/utils v1.1.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.8 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/otiai10/copy v1.14.0 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.52.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/iancoleman/strcase v0.3.0
+	github.com/otiai10/copy v1.14.0
+)
+
+require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/iancoleman/strcase"
 	"github.com/otiai10/copy"
 )
 
@@ -27,6 +28,9 @@ func build() error {
 	}
 	if err := t.ExecuteTemplate(f, "index.html", nil); err != nil {
 		return fmt.Errorf("failed to execute index template: %w", err)
+	}
+	if err := buildHorses(t, outDir); err != nil {
+		return fmt.Errorf("failed to build horses templates: %w", err)
 	}
 	if err := copy.Copy("assets", fmt.Sprintf("%s/assets", outDir)); err != nil {
 		return fmt.Errorf("failed to copy assets dir: %w", err)
@@ -56,4 +60,70 @@ func main() {
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func buildHorses(tmplGlob *template.Template, outDir string) error {
+	if err := os.MkdirAll(fmt.Sprintf("%s/horses", outDir), 0755); err != nil {
+		return fmt.Errorf("failed to create horses dir: %w", err)
+	}
+
+	// temporary hardcoded horse
+	horse := Horse{Name: "Link"}
+	horse.NewImage(
+		"link_headshot.jpeg",
+		"link_headshot_thumb.jpeg",
+		"Headshot of a silver bay Gypsian colt",
+	)
+	horse.NewImage(
+		"link_standing.jpeg",
+		"link_standing_thumb.jpeg",
+		"A silver bay Gypsian colt standing in a field",
+	)
+	horse.NewImage(
+		"link_stepping.jpeg",
+		"link_stepping_thumb.jpeg",
+		"A silver bay Gypsian colt, appearing to step near a Norwegian Fjord mare with her ears pinned",
+	)
+	horse.NewImage(
+		"link_trotting.jpeg",
+		"link_trotting_thumb.jpeg",
+		"A silver bay Gypsian colt trotting in a field",
+	)
+	filename := horse.HTMLPath()
+	f, err := os.Create(fmt.Sprintf("%s/%s", outDir, filename))
+	if err != nil {
+		return fmt.Errorf("failed to create %s: %w", filename, err)
+	}
+	if err := tmplGlob.ExecuteTemplate(f, "horse.html", horse); err != nil {
+		return fmt.Errorf("failed to execute horse template: %w", err)
+	}
+	return nil
+}
+
+type Horse struct {
+	Name   string
+	Images []*Image
+}
+
+func (h *Horse) HTMLPath() string {
+	return fmt.Sprintf("%s.html", strcase.ToSnake(h.Name))
+}
+
+func (h *Horse) NewImage(full, thumbnail, alt string) {
+	prefix := fmt.Sprintf("assets/images/horses/%s", h.Name)
+	img := &Image{
+		Full:      fmt.Sprintf("%s/%s", prefix, full),
+		Thumbnail: fmt.Sprintf("%s/%s", prefix, thumbnail),
+		Alt:       alt,
+	}
+	if h.Images == nil {
+		h.Images = make([]*Image, 0)
+	}
+	h.Images = append(h.Images, img)
+}
+
+type Image struct {
+	Full      string
+	Alt       string
+	Thumbnail string
 }
