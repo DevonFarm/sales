1d74927c8d293b3fa5f136642d7dd96e1b06c521
Clark Winters <github@clarkwinters.com>
2025-08-30 21:11:23 -0500

    WIP: create farm and user funcs

--- farm/farm.go
+++ farm/farm.go
@@ -1,10 +1,36 @@
 package farm
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/DevonFarm/sales/database"
+)
 
 type Farm struct {
 	ID   uuid.UUID `db:"id"`
 	Name string    `db:"name"`
+}
+
+func NewFarm(ctx context.Context, name string, db *database.DB) (*Farm, error) {
+	farm := &Farm{
+		Name: name,
+	}
+	if err := farm.Save(ctx, db); err != nil {
+		return nil, fmt.Errorf("failed to save farm: %w", err)
+	}
+	return farm, nil
+}
+
+func (f *Farm) Save(ctx context.Context, db *database.DB) error {
+	row := db.QueryRow(
+		ctx,
+		`INSERT INTO farms (name) VALUES ($1) RETURNING id`,
+		f.Name,
+	)
+	return row.Scan(&f.ID)
 }
 
 type User struct {
@@ -14,7 +40,31 @@ type User struct {
 	FarmID uuid.UUID `db:"farm_id"`
 }
 
+func (f *Farm) NewUser(ctx context.Context, name, email string, db *database.DB) (*User, error) {
+	user := &User{
+		Name:   name,
+		Email:  email,
+		FarmID: f.ID,
+	}
+	if err := user.Save(ctx, db); err != nil {
+		return nil, fmt.Errorf("failed to save user: %w", err)
+	}
+	return user, nil
+}
+
+func (u *User) Save(ctx context.Context, db *database.DB) error {
+	row := db.QueryRow(
+		ctx,
+		`INSERT INTO users (name, email, farm_id) VALUES ($1, $2, $3) RETURNING id`,
+		u.Name,
+		u.Email,
+		u.FarmID,
+	)
+	return row.Scan(&u.ID)
+}
+
 func GetFarmByUser(userID uuid.UUID) (*Farm, error) {
 	// TODO: implement database query to get farm by user ID
+
 	return nil, nil
 }
